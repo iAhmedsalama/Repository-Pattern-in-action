140c53cbf768fdf9233814431aed0bae9fb3c838
Ahmed Salama <ahmed.s.khalil@hotmail.com>
2022-02-03 17:58:40 +0200

    init commit

---
+++ RepositoryPattern.EF/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RepositoryPattern.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryPattern.EF
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+        }
+        public DbSet<Book> Books{ get; set; }
+        public DbSet<Author> Authors { get; set; }
+    }
+}
